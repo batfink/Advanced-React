c228da98de8853933a19b227629c95ed9457b6b3
batfink <vnystad@gmail.com>
2018-11-03 22:39:59 +0000

    chore: work in progress

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,7 @@
+const Home = () => (
+    <div>
+        <h1>Tjobing!</h1>
+    </div>
+)
+
+export default Home
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,7 @@
+const Sell = () => (
+    <div>
+        <h1>Sell stuff!</h1>
+    </div>
+)
+
+export default Sell
